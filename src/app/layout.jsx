f25bd774a407679c6e4aff7eb1016b4d0a75c266
Eejay <juancarlos.deborja@gmail.com>
2025-03-17 11:56:05 -0500

    Analytics Deployment
    Analytics Deployment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import { Analytics } from "@vercel/analytics/react"
 
 import '@/styles/tailwind.css'
 
@@ -21,6 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <Analytics/>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
